c00788adc77067f7194f57a9128648c24d82632e
Sam Brenner <samuelbrenner@brandeis.edu>
2021-10-15 19:40:07 -0400

    adding search functionality for about page, fixing merge conflict

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,14 +1,13 @@
+require './app/helpers/about_helper'
+
 class AboutController < ApplicationController
   def index
     search_terms = params[:search_param]
     file = File.open('./app/views/about/data/data.txt')
     about = file.read
     if search_terms
-      search_terms = search_terms.downcase
-      relative = about.downcase
-      location = relative.index(search_terms)
-      relative = "#{relative[0 , location]}<mark>#{about[location, location + search_terms.size]}</mark>#{relative[location + search_terms.size..-1]}"
-      puts relative
+      # relative = "#{relative[0..location]}<mark>#{about[location - 1..location + search_terms.size]}</mark>#{relative[location + search_terms.size..-1]}"
+      about = AboutHelper.highlight_instances about, search_terms
     end
     @about = about
   end
