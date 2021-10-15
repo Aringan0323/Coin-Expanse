d04a3c523dc3815bc0cef85486d19f8a60baab0d
Sam Brenner <samuelbrenner@brandeis.edu>
2021-10-15 15:21:19 -0400

    adding basic search logic

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,2 +1,15 @@
 class AboutController < ApplicationController
+  def index
+    search_terms = params[:search_param]
+    file = File.open('./app/views/about/data/data.txt')
+    about = file.read
+    if search_terms
+      search_terms = search_terms.downcase
+      relative = about.downcase
+      location = relative.index(search_terms)
+      relative = "#{relative[0 , location]}<mark>#{about[location, location + search_terms.size]}</mark>#{relative[location + search_terms.size..-1]}"
+      puts relative
+    end
+    @about = about
+  end
 end
