2c21f5998500776f717733f6b4b3d343073f695b
samuelbrenner <54922906+sbrenner132@users.noreply.github.com>
2021-10-16 21:36:46 -0400

    Add Search Functionality for About Page (#2)
    * adding basic search logic
    
    * adding search functionality for about page, fixing merge conflict
    
    * editing highlight styles and taking care of edge case with empty input

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,2 +1,13 @@
+require './app/helpers/about_helper'
+
 class AboutController < ApplicationController
+  def index
+    search_terms = params[:search_param]
+    file = File.open('./app/views/about/data/data.txt')
+    about = file.read
+    if search_terms && !search_terms.empty?
+      about = AboutHelper.highlight_instances about, search_terms
+    end
+    @about = about
+  end
 end
--- db/seeds/coins.seeds.rb
+++ db/seeds/coins.seeds.rb
@@ -1,7 +1,6 @@
 require 'json'
 require 'uri'
 require 'net/http'
-<<<<<<< HEAD
 require './app/helpers/coin_helper.rb'
 require './app/helpers/market_api.rb'
 
@@ -50,32 +49,3 @@ end
 # end
 
 # createCoinsFromSymbols
-=======
-
-require './app/helpers/coin_helper'
-
-Coin.delete_all
-
-def createCoinsFromSymbols
-  info_uri = URI('https://api.binance.us/api/v3/exchangeInfo')
-  info_res = Net::HTTP.get_response(info_uri)
-  puts info_res
-  if info_res.is_a?(Net::HTTPSuccess)
-    symbols_info = JSON(info_res.body)['symbols']
-
-    symbols_info.each do |symbol_info|
-      spot = symbol_info['isSpotTradingAllowed']
-
-      has_usd_conversion = symbol_info['quoteAsset'] == 'USD'
-
-      next if spot && has_usd_conversion
-
-      coin = Coin.create(symbol: symbol_info['baseAsset'])
-      CoinHelper.getTicker(coin)
-      CoinHelper.getDaySummary(coin)
-    end
-  end
-end
-
-createCoinsFromSymbols
->>>>>>> 1f678f9a41785a41ea2099d0db257b86d8d15bed
