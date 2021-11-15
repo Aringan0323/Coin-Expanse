390beb8818f2d73ee7abd54fdb45ffdb7c547b81
sbrenner132 <samuelbrenner@brandeis.edu>
2021-11-15 10:46:32 -0500

    logic kinda working

--- app/controllers/strategies_controller.rb
+++ app/controllers/strategies_controller.rb
@@ -1,4 +1,9 @@
 class StrategiesController < PrivateController
-  def new; end
+  def new
+    respond_to do |format|
+      format.html { render :action => 'new' }
+      format.js { render :action => 'new' }
+    end
+  end
   def show; end
 end
