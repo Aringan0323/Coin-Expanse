eddb22d0eeaac4fe2c45d7fb551ad616dda913df
samuelbrenner <54922906+sbrenner132@users.noreply.github.com>
2021-12-01 16:07:28 -0500

    Order Form (#15)
    * starting profile work
    
    * adding order form

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,16 +1,32 @@
-class OrderController < ApplicationController
+class OrderController < PrivateController
+  include UserHelper
+  include SessionsHelper
+  def index
+    @coins = Coin.all
+  end
 
-    def index
-        @coins = Coin.all
-    end 
-
-    def buy
-
+  def order
+    require_login
+    begin
+      coin = Coin.find(params[:coin_id])
+      qty = params[:quantity].to_f
+      if params[:buy]
+        OrderApi.buy(current_user, coin, qty)
+      else
+        OrderApi.sell(current_user, coin, qty)
+      end
+      flash[:notice] =
+        "You successfully #{params[:buy] ? 'bought' : 'sold'} #{qty} #{pluralize(qty, 'coin')} of #{coin.name}"
+      redirect_to root_url
+    rescue Binance::Api::Error::NewOrderRejected
+      flash[:danger] = 'Insufficient funds to make this order'
+      redirect_to '/order'
+    rescue Binance::Api::Error::InvalidQuantity
+      flash[:danger] = 'Please enter an quantity greater than 0'
+      redirect_to '/order'
+    rescue Binance::Api::Error::IllegalChars
+      flash[:danger] = 'Please enter a valid quantity'
+      redirect_to '/order'
     end
-
-    def sell
-    
-    end
-
-
+  end
 end
--- db/seeds/users.seeds.rb
+++ db/seeds/users.seeds.rb
@@ -5,5 +5,5 @@ require 'date'
 puts("Deleting all users")
 User.delete_all
 
-User.create(username: "admin", email: "test@gmail.com", password: "pass", binance_public_key: "zsXP0MQ3XUQ3clwfewN45dLm0xcBlks0QokSUsLaxT9PkvXMiarAhXYTxy7sHRMi", encryptedBinanceApiKey: "f2xDJ4YJSLUrddqvpk5nQg62XfD8gcPMsikOpkPPhRxB5iKYTf2LXf2tnOLcx8cn")
+User.create(full_name: "Psuedo User", username: "admin", email: "test@gmail.com", password: "pass", binance_public_key: "zsXP0MQ3XUQ3clwfewN45dLm0xcBlks0QokSUsLaxT9PkvXMiarAhXYTxy7sHRMi", encryptedBinanceApiKey: "f2xDJ4YJSLUrddqvpk5nQg62XfD8gcPMsikOpkPPhRxB5iKYTf2LXf2tnOLcx8cn")
 puts "Admin user created"
