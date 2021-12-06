a65a758cccf30b737642e4e90d1f97caf8a24b98
Adam Ring <adamring@brandeis.edu>
2021-12-06 01:21:24 -0500

    validates article uniqueness

--- app/models/news_article.rb
+++ app/models/news_article.rb
@@ -1,6 +1,6 @@
 class NewsArticle < ApplicationRecord
 
-    validates :title, presence: true
+    validates :title, presence: true, uniqueness: true
     validates :name, presence: true
     validates :description, presence: true
     validates :url, presence: true
