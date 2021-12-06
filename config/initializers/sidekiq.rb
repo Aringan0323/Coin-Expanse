a99f32f60b5e24654813f575b6dd28c24d6cb134
Adam Ring <adamring@brandeis.edu>
2021-12-05 21:40:49 -0500

    added sidekiq scheduler

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,7 +6,6 @@ if Rails.env.production?
     Sidekiq.configure_server do |config|
       config.redis = { url: ENV['REDIS_URL']}
     end
-  end
 else
     Sidekiq.configure_server do |config|
         config.redis = { url: 'redis://localhost:6379/0'}
