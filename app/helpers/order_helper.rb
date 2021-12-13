cbd313c822a08a8eee52d534e597456d0ebc2c65
Sam Brenner <samuelbrenner@brandeis.edu>
2021-12-13 12:19:55 -0500

    fixed order page with updating tickers

--- app/helpers/order_helper.rb
+++ app/helpers/order_helper.rb
@@ -1,6 +1,6 @@
 module OrderHelper
   def createData
-    Coin.includes(:book_tickers).pluck(:book_tickers).map { |ticker|
+    Coin.includes(:book_tickers).pluck(:book_tickers)[-4..-1].map { |ticker|
       ticker[1..-2].split(',').select.with_index { |e, i| e if [3, 5].include? i }
     }
   end
