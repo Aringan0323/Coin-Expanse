186118a1a18065b3486dbb8f7bdaf04cc065849c
samuelbrenner <54922906+sbrenner132@users.noreply.github.com>
2021-12-12 23:16:06 -0500

    Order Page Updates (#21)
    * modifying order form
    
    * more changes
    
    * finished changes
    
    * remove console.logs

---
+++ app/helpers/order_helper.rb
@@ -0,0 +1,7 @@
+module OrderHelper
+  def createData
+    Coin.includes(:book_tickers).pluck(:book_tickers).map { |ticker|
+      ticker[1..-2].split(',').select.with_index { |e, i| e if [3, 5].include? i }
+    }
+  end
+end
