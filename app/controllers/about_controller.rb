51852be6b8cbe9044391af6f9c604cbbb257e4ed
adamring <adamring@brandeis.edu>
2021-12-13 23:25:44 -0500

    some formatting to about page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,13 +1,4 @@
-require './app/helpers/about_helper'
-
-class AboutController < PrivateController
-  def index
-    search_terms = params[:search_param]
-    file = File.open('./app/views/about/data/data.txt')
-    about = file.read
-    if search_terms && !search_terms.empty?
-      about = AboutHelper.highlight_instances about, search_terms
+class AboutController < ApplicationController
+    def index
     end
-    @about = about
-  end
 end
